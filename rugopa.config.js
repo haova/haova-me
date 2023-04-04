9fefcce6e82ac82bfec53e12b712117dd3626a83
Vu Anh Hao <hi@haova.me>
2023-04-04 10:54:50 +0700

    Deploy web

--- rugopa.config.js
+++ rugopa.config.js
@@ -1,13 +1,5 @@
 export default {
   sourceDir: './src',
-  assets: ['index.css', 'favicons', 'images', 'icons'],
-  templates: [
-    'parts/header.ejs',
-    'parts/footer.ejs',
-    'parts/navigator.ejs',
-    'index.html',
-    'cv.html',
-  ],
   routes: [
     {
       method: 'get',
